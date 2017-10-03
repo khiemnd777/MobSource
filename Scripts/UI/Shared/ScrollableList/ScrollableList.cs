e9c2eeff3166ad898b9b4718cba9b2767e99044d
Khiemnd <khiemnd777@gmail.com>
2017-10-03 08:26:01 +0700

    concept of lobby and enhancement of ScrollableList

--- Scripts/UI/Shared/ScrollableList/ScrollableList.cs
+++ Scripts/UI/Shared/ScrollableList/ScrollableList.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 
 namespace Mob
 {
+	public enum ScrollableType{
+		Veritical, Horizontal
+	}
 	public class ScrollableList : MobBehaviour 
 	{
+		public ScrollableType scrollableType;
 		public Padding padding;
 		public ScrollRect scrollRect;
 
@@ -39,17 +43,26 @@ namespace Mob
 		}
 
 		public void Refresh(){
-			NeatlyItems ();
-			_rectTrans.SetTopPosition (Vector2.zero);
+			switch(scrollableType){
+				default:
+				case ScrollableType.Veritical:
+					VerticalNeatlyItems ();
+					_rectTrans.SetTopPosition (Vector2.zero);
+					break;
+				case ScrollableType.Horizontal:
+					HorizontalNeatlyItems ();
+					_rectTrans.SetLeftTopPosition(Vector2.zero);
+					break;
+			}
 		}
 
-		void NeatlyItems () {
+		void VerticalNeatlyItems () {
 			ScrollableListItem prevItem = null;
 			var totalItemHeight = 0f;
 			var index = 0;
 			var items = GetItems ();
 			foreach (var item in items) {
-				var itemPaddingBottom = index == items.Length  ? 0f : item.padding.Bottom;
+				var itemPaddingBottom = index == items.Length  ? 0f : item.padding.Bottom + padding.Bottom;
 				var itemHeight = item.GetComponent<RectTransform> ().rect.height + itemPaddingBottom;
 				if (prevItem != null) {
 					var nextPosition = prevItem.transform.position - new Vector3 (0f, itemHeight, 0f);
@@ -60,6 +73,25 @@ namespace Mob
 				++index;
 			}
 			_rectTrans.SetHeight(totalItemHeight);
+		}
+
+		void HorizontalNeatlyItems () {
+			ScrollableListItem prevItem = null;
+			var totalItemWidth = 0f;
+			var index = 0;
+			var items = GetItems ();
+			foreach (var item in items) {
+				var itemPaddingRight = index == items.Length  ? 0f : item.padding.Right + padding.Right;
+				var itemWidth = item.GetComponent<RectTransform> ().rect.width + itemPaddingRight;
+				if (prevItem != null) {
+					var nextPosition = prevItem.transform.position + new Vector3 (itemWidth, 0f, 0f);
+					item.transform.position = nextPosition;
+				}
+				prevItem = item;
+				totalItemWidth += itemWidth;
+				++index;
+			}
+			_rectTrans.SetWidth(totalItemWidth);
 		}
 
 //		void Update() {
