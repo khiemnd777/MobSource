fdb65666ff2a1fe7af46e21dc9af3f0f2da7ab9b
Khiemnd <khiemnd777@gmail.com>
2017-10-06 07:42:56 +0700

    add git.ignore

--- Scripts/Controllers/PlayerJoiner.cs
+++ Scripts/Controllers/PlayerJoiner.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
 using UnityEngine.Networking.Types;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -9,30 +11,43 @@ namespace Mob
 {
     public class PlayerJoiner : MobBehaviour
     {
-        NetworkManager manager;
+        NetworkLobbyManager manager;
+        [SerializeField]
+        Button createBattleBtn;
 
         void Start()
         {
-            manager = NetworkManager.singleton;
-            manager.StartMatchMaker();
-            manager.matchMaker.ListMatches(0, 20, "", true, 0, 0, manager.OnMatchList);
-            StartCoroutine (joinOrCreate ());
+            manager = (NetworkLobbyManager)NetworkManager.singleton;
+
+            createBattleBtn.onClick.AddListener(()=>{
+                manager.StartMatchMaker();
+                manager.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
+                // StartCoroutine (joinOrCreate ());
+            });
         }
 
-        public IEnumerator joinOrCreate() {
-            //waits for response from list matches
-            yield return new WaitForSeconds (5f);
+        public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+        {
             Debug.Log("Done waiting");
             if (manager.matchInfo == null) {
-                Debug.Log("Join or create?");
-                if (manager.matches.Count == 0) {
+                if (matches.Count == 0) {
                     Debug.Log("Create");
                     manager.matchMaker.CreateMatch (manager.matchName, manager.matchSize, true, "", "", "", 0, 0, manager.OnMatchCreate);
                 } else {
                     Debug.Log ("Joining");
-                    manager.matchMaker.JoinMatch (manager.matches[0].networkId, "", "", "", 0, 0, manager.OnMatchJoined);
+                    manager.matchMaker.JoinMatch (matches[0].networkId, "", "", "", 0, 0, manager.OnMatchJoined);
                 }
             }
+        }
+
+        public void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+        {
+            manager.matchInfo = matchInfo;
+        }
+
+        public void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+        {
+            manager.matchInfo = matchInfo;
         }
     }
 }
