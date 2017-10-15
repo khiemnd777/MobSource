011bd8b5a52c1fd2312f18b98307f5de95b468bb
Khiemnd <khiemnd777@gmail.com>
2017-10-15 10:26:19 +0700

    Finding an appropriate battle

--- Scripts/Networking/MobNetworkLobbyManager.cs
+++ Scripts/Networking/MobNetworkLobbyManager.cs
@@ -9,7 +9,8 @@ namespace Mob
 {
     public class MobNetworkLobbyManager : NetworkLobbyManager
     {
-        static Dictionary<int, int> currentPlayers = new Dictionary<int, int>();
+        public CharacterType characterType;
+        public PlayerState playerState;
 
         public override bool OnLobbyServerSceneLoadedForPlayer(GameObject lobbyPlayer, GameObject gamePlayer){
             // Debug.Log("OnLobbyServerSceneLoadedForPlayer");
@@ -19,54 +20,218 @@ namespace Mob
             return true;
         }
 
-        public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
-        {
-            base.OnServerAddPlayer(conn, playerControllerId);
+        #region Starting a Host or a Client 
+
+        public override void OnStartHost(){
+            Debug.Log("OnStartHost");
+            playerState = PlayerState.WaitingConnection;
+            base.OnStartHost();
         }
+
+        // public override void OnLobbyStartHost(){
+        //     Debug.Log("OnLobbyStartHost");
+        //     base.OnLobbyStartHost();
+        // }
+
+        // public override void OnLobbyStartServer(){
+        //     Debug.Log("OnLobbyStartServer");
+        //     base.OnLobbyStartServer();
+        // }
+
+        // public override void OnServerConnect(NetworkConnection conn)
+        // {
+        //     Debug.Log("OnServerConnect");
+        //     base.OnServerConnect(conn);
+        // }
+
+        // public override void OnLobbyServerConnect(NetworkConnection conn){
+        //     Debug.Log("OnLobbyServerConnect");
+        //     base.OnLobbyServerConnect(conn);
+        // }
+
+        // public override void OnStartClient(NetworkClient lobbyClient)
+        // {
+        //     Debug.Log("OnStartClient");
+        //     playerState = PlayerState.FindingAppropriateBattle;
+        //     base.OnStartClient(lobbyClient);
+        // }
+
+        // public override void OnLobbyStartClient(NetworkClient lobbyClient){
+        //     Debug.Log("OnLobbyStartClient");
+        //     base.OnLobbyStartClient(lobbyClient);
+        // }
+
+        // public override void OnClientConnect(NetworkConnection conn){
+        //     Debug.Log("OnClientConnect");
+        //     base.OnClientConnect(conn);
+        // }
+
+        // public override void OnLobbyClientConnect(NetworkConnection conn){
+        //     Debug.Log("OnLobbyClientConnect");
+        //     base.OnLobbyClientConnect(conn);
+        // }
+
+        // public override void OnLobbyClientEnter(){
+        //     Debug.Log("OnLobbyClientEnter");
+        //     base.OnLobbyClientEnter();
+        // }
+
+        // public override void OnServerReady(NetworkConnection conn){
+        //     Debug.Log("OnServerReady");
+        //     base.OnServerReady(conn);
+        // }
+
+        // public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+        // {
+        //     Debug.Log("OnServerAddPlayer");
+        //     base.OnServerAddPlayer(conn, playerControllerId);
+        // }
 
         public override GameObject OnLobbyServerCreateLobbyPlayer(NetworkConnection conn, short playerControllerId)
         {
-            if (!currentPlayers.ContainsKey(conn.connectionId))
-                currentPlayers.Add(conn.connectionId, 0);
-            
-            return base.OnLobbyServerCreateLobbyPlayer(conn, playerControllerId);
+            Debug.Log("OnLobbyServerCreateLobbyPlayer");
+            var lobbyGo = GameObject.Instantiate(lobbyPlayerPrefab, Vector3.zero, Quaternion.identity);
+            var lobbyPlayer = lobbyGo.GetComponent<MobNetworkLobbyPlayer>();
+            lobbyPlayer.characterType = characterType;
+            return lobbyGo.gameObject;
         }
+
+        #endregion
+
+        public override void OnDropConnection(bool success, string extendedInfo){
+            Debug.Log("OnDropConnection");
+            switch(playerState){
+                default:
+                case PlayerState.Unknown:
+                case PlayerState.Exiting:
+                case PlayerState.InBattle:
+                    base.OnDropConnection(success, extendedInfo);
+                break;
+            }
+        }
+
+        // public override void OnClientNotReady(NetworkConnection conn){
+        //     Debug.Log("OnClientNotReady");
+        //     base.OnClientNotReady(conn);
+        // }
+
+        // public override void OnLobbyClientAddPlayerFailed(){
+        //     Debug.Log("OnLobbyClientAddPlayerFailed");
+        //     base.OnLobbyClientAddPlayerFailed();
+        // }
+
+        // public override void OnStopHost(){
+        //     Debug.Log("OnStopHost");
+        //     base.OnStopHost();
+        // }
+
+        // public override void OnLobbyClientExit(){
+        //     Debug.Log("OnLobbyClientExit");
+        //     base.OnLobbyClientExit();
+        // }
+    
+        public override void OnClientDisconnect(NetworkConnection conn){
+            Debug.Log("OnClientDisconnect");
+            switch(playerState){
+                default:
+                case PlayerState.Unknown:
+                case PlayerState.Exiting:
+                case PlayerState.InBattle:
+                    base.OnClientDisconnect(conn);
+                break;
+                case PlayerState.WaitingConnection:
+                case PlayerState.FindingAppropriateBattle:
+                    PlayerMatchMaker.instance.StartMatchMaker();
+                    PlayerMatchMaker.instance.GetMatchList(0, 20, 0, 0, matches => {
+                        PlayerMatchMaker.instance.CreateOrJoinMatch(matches, 2, "", 0, 0);
+                    });
+                break;
+            }
+        }
+
+        public override void OnClientError(NetworkConnection conn, int errorCode){
+            Debug.Log("OnClientError");
+            playerState = PlayerState.Unknown;
+            base.OnClientError(conn, errorCode);
+        }
+
+        // public override void OnLobbyServerPlayerRemoved(NetworkConnection conn, short playerControllerId){
+        //     Debug.Log("OnLobbyServerPlayerRemoved");
+        //     base.OnLobbyServerPlayerRemoved(conn, playerControllerId);
+        // }
+
+        public override void OnLobbyStopHost(){
+            Debug.Log("OnLobbyStopHost");
+            playerState = PlayerState.Unknown;
+            base.OnLobbyStopHost();
+        }
+
+        // public override void OnLobbyClientDisconnect(NetworkConnection conn){
+        //     Debug.Log("OnLobbyClientDisconnect");
+        //     base.OnLobbyClientDisconnect(conn);
+        // }
+
+        // public override void OnLobbyStopClient(){
+        //     Debug.Log("OnLobbyStopClient");
+        //     base.OnLobbyStopClient();
+        // }
+
+        // public override void OnStopServer(){
+        //     Debug.Log("OnStopServer");
+        //     base.OnStopServer();
+        // }
 
         public override GameObject OnLobbyServerCreateGamePlayer(NetworkConnection conn, short playerControllerId)
         {
-            int index = currentPlayers[conn.connectionId];
-
-            var _temp = (GameObject)GameObject.Instantiate(spawnPrefabs[0], Vector3.zero,
-                Quaternion.identity);
-            var swordmand = _temp.GetComponent<Swordman>();
-            swordmand.DefaultValue();
-            swordmand.GetModule<GoldModule>(x =>
-            {
-                x.AddGold(10);
-            });
-            swordmand.GetModule<EnergyModule>(x =>
-            {
-                x.maxEnergy = 12;
-                x.AddEnergy(0);
-            });
-            swordmand.GetModule<StatModule>(x =>
-            {
-                x.point = 20;
-            });
-
-            // NetworkServer.AddPlayerForConnection(conn, _temp, playerControllerId);
-            return _temp;
+            switch(characterType){
+                default:
+                case CharacterType.Swordman:
+                case CharacterType.Mage:
+                case CharacterType.Berserker:
+                    return CreateCharacter<Swordman>(c => 
+                    {
+                        c.DefaultValue();
+                        c.GetModule<GoldModule>(x =>
+                        {
+                            x.AddGold(10);
+                        });
+                        c.GetModule<EnergyModule>(x =>
+                        {
+                            x.maxEnergy = 12;
+                            x.AddEnergy(0);
+                        });
+                        c.GetModule<StatModule>(x =>
+                        {
+                            x.point = 20;
+                        });
+                    });
+            }
         }
 
         public override void OnLobbyServerPlayersReady(){
             var allready = lobbySlots.Count(x => !x.IsNull() && x.readyToBegin) == maxPlayers;
-            if (allready)
+            if (allready){
+                playerState = PlayerState.InBattle;
                 StartCoroutine(ServerCountdownCoroutine());
+            }
         }
+
+        GameObject CreateCharacter<T>(Action<T> predicate = null) where T : Race
+		{
+			var prefabObj = spawnPrefabs.SingleOrDefault(x => x.GetComponent<T>() != null);
+			if(prefabObj.IsNull())
+				return null;
+            var go = GameObject.Instantiate(spawnPrefabs[0], Vector3.zero,
+                Quaternion.identity);
+            var character = go.GetComponent<T>();
+            if(predicate != null)
+                predicate.Invoke(character);
+            return go;
+		}
 
         IEnumerator ServerCountdownCoroutine()
         {
-            var remainingTime = 5f;
+            var remainingTime = 3f;
             var floorTime = Mathf.FloorToInt(remainingTime);
 
             while (remainingTime > 0)
@@ -97,7 +262,6 @@ namespace Mob
                     (lobbySlots[i] as MobNetworkLobbyPlayer).RpcUpdateCountdown(0);
                 }
             }
-
             ServerChangeScene(playScene);
         }
 
